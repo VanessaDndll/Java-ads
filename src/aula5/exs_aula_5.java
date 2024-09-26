d7d268a797ae4cffd1c4e783904d1c2352fe1736
Vanessa <vanessadonadelli2014@hotmail.com>
2024-09-26 19:52:33 -0300

    Exercícios aula 5 - Interface

---
+++ src/aula5/exs_aula_5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula5;
+
+/**
+ *
+ * @author Adriano
+ */
+public class exs_aula_5 {
+    public static void main(String[] args) {
+        telaprincipal tp = new telaprincipal();
+        tp.setVisible(true);
+    }
+}
